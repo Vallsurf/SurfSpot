e2b21a492b3df6e588670b469c8314e90b084bc1
Val Singhal <Vallsurf@gmail.com>
2018-09-06 17:23:05 -0700

    updated config

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-exports.API_BASE_URL= process.env.REACT_APP_API_BASE_URL ||"http://localhost:8080";
+exports.API_BASE_URL= (window.location.href.indexOf("localhost") > -1 ) ? "http://localhost:8080" : "https://thawing-reef-45020.herokuapp.com";
+
